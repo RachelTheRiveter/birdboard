8b96fbf4ae5e274bb4740cc6e34976cbae7e98af
RachelTheRiveter <theholthut@gmail.com>
2020-01-04 21:15:58 -0700

    Episode 3 - Testing Request Validation - Completed

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -27,4 +27,28 @@ class ProjectsTest extends TestCase
 
         $this->get('/projects')->assertSee($attributes['title']);
     }
+
+    /** @test */
+
+    public function a_project_requires_a_title()
+    {
+        // create - build out an attribute and save to db as an object,
+        // make will build it , but not save it as an object,
+        // raw build out attribute, but save it as an array
+
+        $attributes = factory('App\Project')->raw(['title' => '']);
+        $this->post('/projects', $attributes)->assertSessionHasErrors('title');
+    }
+
+    /** @test */
+
+    public function a_project_requires_a_description()
+    {
+        // create - build out an attribute and save to db as an object,
+        // make will build it , but not save it as an object,
+        // raw build out attribute, but save it as an array
+
+        $attributes = factory('App\Project')->raw(['description' => '']);
+        $this->post('/projects', $attributes)->assertSessionHasErrors('description');
+    }
 }
