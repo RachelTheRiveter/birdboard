8e8449fd23d57577d28fe14cd46d415ba7c6f05b
RachelTheRiveter <theholthut@gmail.com>
2020-01-05 01:30:30 -0700

    Episode 6 - Scoping Projects Complete

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -12,11 +12,28 @@ class ProjectsTest extends TestCase
 
     /** @test */
 
-    public function only_authenticated_users_can_create_projects()
+    public function guests_cannot_create_projects()
     {
         $attributes = factory ('App\Project')->raw(['owner_id' => null]);
 
         $this->post('/projects', $attributes)->assertRedirect('login');
+
+    }
+
+    /** @test */
+
+    public function guests_may_not_view_projects()
+    {
+        $this->get('/projects')->assertRedirect('login');
+    }
+
+    /** @test */
+
+    public function guests_can_not_view_a_single_project()
+    {
+        $project = factory('App\Project')->create();
+
+        $this->get($project->path())->assertRedirect('login');
 
     }
 
@@ -43,15 +60,30 @@ class ProjectsTest extends TestCase
 
     /** @test */
 
-    public function a_user_can_view_a_project()
+    public function a_user_can_view_their_project()
     {
+        $this->be(factory('App\User')->create());
+
         $this->withoutExceptionHandling();
 
-        $project = factory('App\Project')->create();
+        $project = factory('App\Project')->create(['owner_id' => auth()->id()]);
 
         $this->get($project->path())
             ->assertSee($project->title)
             ->assertSee($project->description);
+    }
+
+    /** @test */
+
+    public function an_authenticated_user_cannot_view_the_projects_of_others()
+    {
+        $this->be(factory('App\User')->create());
+
+//        $this->withoutExceptionHandling();
+
+        $project = factory('App\Project')->create();
+
+        $this->get($project->path())->assertStatus(403);
     }
 
     /** @test */
